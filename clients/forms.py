5d1c90ea063a995e6af4129ad44dab3f5a4d0f4e
jwogrady <john@status26.com>
2024-07-09 16:26:27 -0500

    rename clients module fields, commit migrations

--- clients/forms.py
+++ clients/forms.py
@@ -4,4 +4,4 @@ from .models import Client
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
-        fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
+        fields = ['first_name', 'last_name', 'email', 'mobile_number', 'mailing_address']
--- clients/models.py
+++ clients/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 class Client(models.Model):
+    """A class to represent a client.
+    """
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15)
-    address = models.TextField()
+    mobile_number = models.CharField(max_length=15)
+    mailing_address = models.TextField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+# TODO: A contact has many profiles.
+# TODO: Validate email, phone, and mailing address
+# TODO: Create a table to link Clients and Aware Contacts
