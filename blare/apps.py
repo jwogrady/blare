88f56d50c286573e0a34c7dc7b619e65cbaae3df
jwogrady <john@status26.com>
2024-07-15 12:54:25 -0500

    reorganize project so all blare models are in one app under a given instance of mysite. Mysite is presumably Orbit, but it could be standalone

--- blare/apps.py
+++ blare/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-class ClientsConfig(AppConfig):
+
+class BlareConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'clients'
+    name = 'blare'
