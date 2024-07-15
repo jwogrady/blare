b113d4fa0b33635a5bdd40a613673dba9e5bceed
jwogrady <john@status26.com>
2024-07-15 13:06:06 -0500

    add blare app

--- blare/views.py
+++ blare/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. This is the Blare index page.")
